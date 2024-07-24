49258a705a549775a83a80147c81f44212ac4de9
吳 蕥綺 <n0975116268@gmail.com>
2024-07-24 15:46:59 +0800

    add pagination

--- jobapp/views.py
+++ jobapp/views.py
@@ -3,15 +3,23 @@ from .models import Job, JobLog
 from django.db.models import Q
 from django.utils.dateparse import parse_datetime
 
+from django.views.generic import ListView
+from .models import Job
+from django.db.models import Q
+from django.utils.dateparse import parse_datetime
+
 class JobListView(ListView):
     model = Job
     template_name = 'jobapp/job_list.html'
     context_object_name = 'jobs'
+    paginate_by = 5
 
     def get_queryset(self):
         queryset = super().get_queryset()
         query = self.request.GET.get('q')
+
         if query:
+            # 处理日期查询
             try:
                 date_query = parse_datetime(query)
                 if date_query:
@@ -32,13 +40,28 @@ class JobListView(ListView):
                     Q(spare_ip__icontains=query) |
                     Q(last_updated__icontains=query)
                 )
+
         return queryset
+
+    def get_paginate_by(self, queryset):
+        paginate_by = self.request.GET.get('paginate_by', self.paginate_by)
+        try:
+            return int(paginate_by)
+        except (TypeError, ValueError):
+            return self.paginate_by
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['paginate_by'] = self.get_paginate_by(self.get_queryset())
+        context['search_query'] = self.request.GET.get('q', '')
+        return context
+
 
 class JobLogListView(ListView):
     model = JobLog
     template_name = 'jobapp/joblog_list.html'
     context_object_name = 'joblogs'
-    paginate_by = 3  
+    paginate_by = 5  # Default value
 
     def get_queryset(self):
         job_id = self.kwargs.get('job_id')
@@ -47,4 +70,12 @@ class JobLogListView(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['job_id'] = self.kwargs.get('job_id')
+        context['paginate_by'] = self.get_paginate_by(self.get_queryset())
         return context
+
+    def get_paginate_by(self, queryset):
+        paginate_by = self.request.GET.get('paginate_by', self.paginate_by)
+        try:
+            return int(paginate_by)
+        except (TypeError, ValueError):
+            return self.paginate_by
