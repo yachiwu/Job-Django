a75d5ebcaa35b6fdaa5c2a2b72c875076eb576e8
吳 蕥綺 <n0975116268@gmail.com>
2024-07-25 16:22:57 +0800

    filter by date

--- jobapp/views.py
+++ jobapp/views.py
@@ -1,12 +1,13 @@
+from datetime import datetime, date, time
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, redirect
+from django.urls import reverse
 from django.views.generic import ListView
 from .models import Job, JobLog
 from django.db.models import Q
-from django.utils.dateparse import parse_datetime
-
-from django.views.generic import ListView
-from .models import Job
-from django.db.models import Q
-from django.utils.dateparse import parse_datetime
+from django.utils.dateparse import parse_date
+from django.utils.timezone import make_aware, get_current_timezone
+import pytz
 
 class JobListView(ListView):
     model = Job
@@ -14,32 +15,57 @@ class JobListView(ListView):
     context_object_name = 'jobs'
     paginate_by = 5
 
+    def get_default_local_date(self):
+        """Return today's date formatted for the date input field."""
+        return date.today().strftime('%Y-%m-%d')
+
+    def convert_to_utc(self, local_datetime):
+        """Convert a local datetime to UTC."""
+        local_timezone = get_current_timezone()
+        aware_datetime = make_aware(local_datetime, timezone=local_timezone)
+        return aware_datetime.astimezone(pytz.utc)
+
+    def get_date_range(self, local_date):
+        """Return the start and end of a local date as UTC."""
+        start_of_day = datetime.combine(local_date, time.min)
+        end_of_day = datetime.combine(local_date, time.max)
+        # Convert to UTC directly
+        start_of_day_utc = self.convert_to_utc(start_of_day)
+        end_of_day_utc = self.convert_to_utc(end_of_day)
+        return start_of_day_utc, end_of_day_utc
+
+    # def get(self, request, *args, **kwargs):
+    #     # If no last_updated parameter, redirect to include today's date
+    #     if not request.GET.get('last_updated'):
+    #         today_date = self.get_default_local_date()
+    #         return HttpResponseRedirect(f"{request.path}?last_updated={today_date}")
+    #     return super().get(request, *args, **kwargs)
+
     def get_queryset(self):
         queryset = super().get_queryset()
-        query = self.request.GET.get('q')
+        last_updated_query = self.request.GET.get('last_updated')
+        search_query = self.request.GET.get('search')
 
-        if query:
-            # 处理日期查询
+        if last_updated_query:
             try:
-                date_query = parse_datetime(query)
-                if date_query:
-                    queryset = queryset.filter(last_updated__date=date_query.date())
-                else:
-                    queryset = queryset.filter(
-                        Q(device_name__icontains=query) |
-                        Q(device_ip__icontains=query) |
-                        Q(spare_name__icontains=query) |
-                        Q(spare_ip__icontains=query) |
-                        Q(last_updated__icontains=query)
-                    )
+                last_updated_parsed = parse_date(last_updated_query)
+                if last_updated_parsed:
+                    start_of_day_utc, end_of_day_utc = self.get_date_range(last_updated_parsed)
+                    queryset = queryset.filter(last_updated__range=(start_of_day_utc, end_of_day_utc))
             except ValueError:
-                queryset = queryset.filter(
-                    Q(device_name__icontains=query) |
-                    Q(device_ip__icontains=query) |
-                    Q(spare_name__icontains=query) |
-                    Q(spare_ip__icontains=query) |
-                    Q(last_updated__icontains=query)
-                )
+                pass
+        # else:
+        #     today_date = date.today()
+        #     start_of_today_utc, end_of_today_utc = self.get_date_range(today_date)
+        #     queryset = queryset.filter(last_updated__range=(start_of_today_utc, end_of_today_utc))
+
+        if search_query:
+            queryset = queryset.filter(
+                Q(device_name__icontains=search_query) |
+                Q(device_ip__icontains=search_query) |
+                Q(spare_name__icontains=search_query) |
+                Q(spare_ip__icontains=search_query)
+            )
 
         return queryset
 
@@ -52,10 +78,14 @@ class JobListView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        queryset = self.get_queryset()
         context['paginate_by'] = self.get_paginate_by(self.get_queryset())
-        context['search_query'] = self.request.GET.get('q', '')
+        context['search_query'] = self.request.GET.get('search', '')
+        # set default last_updated_query to today
+        # context['last_updated_query'] = self.request.GET.get('last_updated', self.get_default_local_date())
+        context['last_updated_query'] = self.request.GET.get('last_updated', '')
+        context['total_count'] = queryset.count()
         return context
-
 
 class JobLogListView(ListView):
     model = JobLog
@@ -69,7 +99,10 @@ class JobLogListView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['job_id'] = self.kwargs.get('job_id')
+        job_id = self.kwargs.get('job_id')
+        job = get_object_or_404(Job, job_id=job_id)
+
+        context['job'] = job
         context['paginate_by'] = self.get_paginate_by(self.get_queryset())
         return context
 
@@ -79,3 +112,4 @@ class JobLogListView(ListView):
             return int(paginate_by)
         except (TypeError, ValueError):
             return self.paginate_by
+
